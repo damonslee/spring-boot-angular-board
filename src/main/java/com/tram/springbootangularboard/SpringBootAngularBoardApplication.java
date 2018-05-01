2f091ffcba33b048f39eba86320cbc1d6a793bc7
KimKyuNam <tramyu@naver.com>
2018-05-01 15:47:19 +0900

    init project

---
+++ src/main/java/com/tram/springbootangularboard/SpringBootAngularBoardApplication.java
@@ -0,0 +1,12 @@
+package com.tram.springbootangularboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularBoardApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tram/springbootangularboard/SpringBootAngularBoardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tram.springbootangularboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootAngularBoardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
