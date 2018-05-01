b3fb8fbbce4e14c8ab86a636e38a2aec55f0ee8c
KimKyuNam <tramyu@naver.com>
2018-05-01 23:11:43 +0900

    create domain, dto

---
+++ src/main/java/com/tram/springbootangularboard/domain/Posts.java
@@ -0,0 +1,36 @@
+package com.tram.springbootangularboard.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name="posts")
+public class Posts {
+    @Id
+    @GeneratedValue
+    @Column(name="post_id")
+    private Long id;
+
+    @Column(name="title", length = 500, nullable = false)
+    private String title;
+
+
+    @Column(name="content", columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(name="author")
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
