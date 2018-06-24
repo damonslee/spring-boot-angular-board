da993a18f4418e002626d2b4a200ac82e8100ee9
KimKyuNam <tramyu@naver.com>
2018-06-24 23:56:47 +0900

    CommonUtils에 ObjectMapper 추가 구조 변경

--- src/main/java/com/tram/springbootangularboard/common/CommonUtils.java
+++ src/main/java/com/tram/springbootangularboard/common/CommonUtils.java
@@ -1,9 +1,15 @@
 package com.tram.springbootangularboard.common;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class CommonUtils {
-    public static BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+    private static ObjectMapper objectMapper = new ObjectMapper();
+    private static BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+    public static BCryptPasswordEncoder getBCryptPasswordEncoder() {
+        return bCryptPasswordEncoder;
+    }
+    public static ObjectMapper getObjectMapper() {
+        return objectMapper;
     }
 }
