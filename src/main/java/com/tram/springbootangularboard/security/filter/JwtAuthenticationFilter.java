f15724388766cd99ed0c2fee3193f0e146a6810a
KimKyuNam <tramyu@naver.com>
2018-07-08 17:11:10 +0900

    JwtAuthenticationFilter 생성, HeaderTokenExtractor 클래스, 테스트 생성

---
+++ src/main/java/com/tram/springbootangularboard/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,26 @@
+package com.tram.springbootangularboard.security.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+    private static final Logger log = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+
+    protected JwtAuthenticationFilter(RequestMatcher requiresAuthenticationRequestMatcher) {
+        super(requiresAuthenticationRequestMatcher);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        return null;
+    }
+}
