614e1d92c96f99ba1baef5eed506783fb5c7b8d6
KimKyuNam <tramyu@naver.com>
2018-08-12 18:39:36 +0900

    Implements navi module feature #20
    core 모듈 생성, 상단 헤더(네비게이션) 컴포넌트 구현

--- src/main/webapp/src/app/core/core.module.ts
+++ src/main/webapp/src/app/core/core.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NaviComponent } from './navi/navi.component';
 import { ClarityModule } from '@clr/angular';
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   imports: [
     CommonModule,
-    ClarityModule
+    ClarityModule,
+    SharedModule
   ],
   declarations: [NaviComponent],
   exports: [NaviComponent, ClarityModule]
