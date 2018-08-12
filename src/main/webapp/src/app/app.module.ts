be8b51f38ebb036760958565b6d9fda9373dbfd2
KimKyuNam <tramyu@naver.com>
2018-08-12 18:34:46 +0900

    Implements home module, componenet feature #20
    메인 페이지 출력을 위한 home module, home component 추가
    메인 app에 router-outlet 및 라우팅 설정 추가
    shared 모듈 추가, routerLink 사용을 위한 RouterModule export 추가

--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -1,9 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Route, RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { BoardModule } from './board/board.module';
+import { HomeModule } from './home/home.module';
+import { HomeComponent } from "./home/home/home.component";
+import { SharedModule } from "./shared/shared.module";
+
+const routes:Routes = [
+  {
+    path: '', component: HomeComponent
+  }
+];
 
 
 @NgModule({
@@ -12,7 +22,11 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
     BrowserModule,
-    CoreModule
+    CoreModule,
+    BoardModule,
+    HomeModule,
+    SharedModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
